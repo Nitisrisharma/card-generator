8c1566fe2158b9fca7c0310a07d1b6ca0ece87be
Nipurnika sharma <165471091+Nitisrisharma@users.noreply.github.com>
2026-06-27 11:44:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+const ai = new GoogleGenAI({ 
+  apiKey: process.env.GEMINI_API_KEY 
+});
 
 app.use(cors());
 app.use(express.json());
